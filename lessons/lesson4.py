afa774d97dd2194d8a1d5b020f426b54d2cbd934
Geek_Tech <geecktech.och2@gmail.com>
2024-07-11 19:59:05 +0600

    'fix_main.py'

---
+++ lessons/lesson4.py
@@ -0,0 +1,40 @@
+# Дектораторы, множественное наследование, ветки в гите
+# Инкапсуляция
+
+class ABC:
+    def __init__(self,a,b,c):
+        self.a=a
+        self._b=b
+        self.__c=c
+
+
+    def isB(self,a):
+        self._setB(a)
+        self._getsB()
+
+    def _getsB(self):
+        print(self._b)
+
+
+    def _setB(self,b):
+        self._b=b
+
+
+
+
+    def __str__(self):
+        return self._b
+
+a=ABC('a','_b',"__c")
+
+a.isB('великий')
+print(a)
+# a._b='newB'
+# print(a._b)
+#
+#
+# print(dir(a))
+# a.__c='ложный си'
+# a._ABC__c='истинный си'
+# print(a.__c)
+# print(a)
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def mtime(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        res=end_time-start_time
+        print(f"Function {func.__name__} took {res:.3f} seconds")
+        return result
+    return wrapper
 
+
+@mtime
+def comsum(n):
+    total = sum(range(n))
+    return total
+
+print(comsum(1000000))
+
+mtime(comsum(10000))
+
+# mtime(comsum(100000))
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
